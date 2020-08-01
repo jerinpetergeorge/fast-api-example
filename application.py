91437dc4509f59d1584cff1a46e271ccd633eadb
Jerin Peter George <jerinpetergeorge@gmail.com>
2020-08-01 20:11:05 +0530

    possible url list added

--- application.py
+++ application.py
@@ -10,7 +10,8 @@ demo_app = FastAPI()
 
 @demo_app.get(path='/', include_in_schema=False)
 def root_api():
-    return {'message': 'this is root api'}
+    url_list = [{'path': route.path, 'name': route.name} for route in demo_app.routes]
+    return {'message': 'All possible URLs in this application', 'result': url_list}
 
 
 demo_app.include_router(hello_world.router, prefix='/demo/hello')
