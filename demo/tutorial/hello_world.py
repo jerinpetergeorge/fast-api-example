724b1f7b165ec21fda75982e109afd3b740a19f9
Jerin Peter George <jerinpetergeorge@gmail.com>
2020-08-25 08:20:02 +0530

    empty logger added

--- demo/tutorial/hello_world.py
+++ demo/tutorial/hello_world.py
@@ -1,8 +1,12 @@
+import logging
 from fastapi import APIRouter
 
 router = APIRouter()
 
+empty_logger = logging.getLogger("")
+
 
 @router.get("/")
 async def read_root():
+    empty_logger.info("This is `read_root()` function")
     return {"msg": "Hello World"}
